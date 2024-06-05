6a96a1e487d1d0e7560019c69a7a1bb1f206f057
Tran Duc Minh <tranducminh261103@gmail.com>
2024-06-05 18:45:06 +0700

    Add function to calculate activation function

---
+++ week_1/activation_functions.py
@@ -0,0 +1,50 @@
+import math
+
+
+def is_number(x):
+    try:
+        float(x)
+    except ValueError:
+        return False
+    return True
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+def relu(x):
+    result = 0
+    if x > 0:
+        result = x
+    return result
+
+
+def elu(x):
+    result = 0
+    alpha = 0.01
+    if x <= 0:
+        result = alpha * (math.exp(x) - 1)
+    else:
+        result = x
+    return result
+
+
+def calc_activation_func():
+    x = input("Input x = ")
+    if not is_number(x):
+        print("x must be a number")
+        return
+    x = float(x)
+    
+    act_name = input("Input activation Function (sigmoid|relu|elu): ").lower()
+    if act_name == 'sigmoid':
+        print(f"{act_name}: f({x}) = {sigmoid(x)}")
+    elif act_name == 'relu':
+        print(f"{act_name}: f({x}) = {relu(x)}")
+    elif act_name == 'elu':
+        print(f"{act_name}: f({x}) = {elu(x)}")
+    else:
+        print(f"{act_name} is not supported")
+        
+calc_activation_func()
